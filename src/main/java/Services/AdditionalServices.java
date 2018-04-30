f874b418da41f670c8f88626d0594a74f44f23f3
Matthew Thorne <mwilsonthorne@gmail.com>
2018-04-30 15:00:59 +0100

    Added and tested a non-abstract method to the abstract class AdditionalService(serviceClosed) and tested

--- src/main/java/Services/AdditionalServices.java
+++ src/main/java/Services/AdditionalServices.java
@@ -10,4 +10,11 @@ public abstract class AdditionalServices {
     //Example of an Abstract method. Abstract methods have no body and must be defined in the child class.
     //Any child class must implement a notInService method which takes a single String parameter and returns a String
 
+    public String serviceClosed(){
+        return "We are now closed";
+    }
+
+    //Example of a method in the abstract class that is implemented from this class. See postOfficeServiceClosed
+    //& hotFoodServiceClosed in test file.
+
 }
--- src/test/java/ServicesTest/AdditionalServicesTest.java
+++ src/test/java/ServicesTest/AdditionalServicesTest.java
@@ -21,6 +21,10 @@ public class AdditionalServicesTest {
 //        additionalServices = new AdditionalServices();
         //We need to have the ability to implement common code with sub classes and not
         // instantiate the AdditionalServices class so we make the AdditionServices Class abstract.
+        additionalServices = new PostOffice("Last Post at 4.30pm today");
+
+        // we can't make an instance of an abstract class but we can declare a variable of the abstract class and
+        //assign it with an instance of its subclass(PostOffice).
 
     }
 
@@ -52,13 +56,34 @@ public class AdditionalServicesTest {
     // instantiate the AdditionalServices class
 
     @Test
-    public void postOfficeCanClose(){
+    public void postOfficeCanBeOutOfService(){
         assertEquals("Back in 5 mins", postOffice.notInService("5 mins"));
     }
 
     @Test
-    public void hotFoodCanClose(){
+    public void hotFoodCanBeOutOfService(){
         assertEquals("Breakfast menu finished. Lunch starts at 2pm", hotFood.notInService("2pm"));
     }
+
+    @Test
+    public void postOfficeCanClose(){
+        assertEquals("We are now closed", postOffice.serviceClosed());
+    }
+
+    @Test
+    public void hotFoodCanClose(){
+        assertEquals("We are now closed", hotFood.serviceClosed());
+    }
+
+    //Above two tests are examples of serviceClosed method which hotFood and postOffice objects call
+    // from the abstract class.
+
+    @Test
+    public void todayAdditionalServicesLastService(){
+        assertEquals("Last Post at 4.30pm today", additionalServices.lastService("4.30pm"));
+    }
+
+    //Example above of testing with a variable of the abstract class.
+
 
 }
