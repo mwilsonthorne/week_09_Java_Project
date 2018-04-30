9ac1e73a67b310861ea1a2933267874be2e2554c
Matthew Thorne <mwilsonthorne@gmail.com>
2018-04-30 15:17:38 +0100

    Completed Open/Closed Principle

--- src/main/java/Services/AdditionalServices.java
+++ src/main/java/Services/AdditionalServices.java
@@ -17,4 +17,16 @@ public abstract class AdditionalServices {
     //Example of a method in the abstract class that is implemented from this class. See postOfficeServiceClosed
     //& hotFoodServiceClosed in test file.
 
+    //SOLID - O (OPEN/CLOSED PRINCIPLE)
+    //The package Services shows our classes are Open for extension and Closed for modification.
+    //Meaning if we wanted to add in another type of Additional Service we don't have to change our AdditionalServices
+    //class. We create a new class and extend the AdditionalServices class.
+    //1. Abstract Classes may or may not contain abstract methods eg methods without body
+    //2. If a class has an abstract method, then the class is declared abstract
+    //3. If a class is abstract it cannot be instantiated
+    //4. To use an abstract class you have to inherit it through a child class of the abstract.
+    //5. If you inherit an abstract class, you have to provide implementations to all the abstract methods in it
+    //6. You can declare variables and method parameters where the type is an abstract class but they have to take
+    //the value of an instance of on of the child classes.
+
 }
